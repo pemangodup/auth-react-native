1aa421f8c8f52548c1b3e6bbf102d26fc3572c8e
Pema Ngodup <pemangodup@Pemas-MBP.Home>
2025-10-20 01:16:54 +0100

    logging user in

--- util/http.js
+++ util/http.js
@@ -2,13 +2,21 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyCmXP9-7k5q-rqec_MgGB7ztL8nXIld-3I";
 
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+  console.log(response);
+}
+
 export async function createUser(email, password) {
-  const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
-    {
-      email,
-      password,
-      returnSecureToken: true,
-    }
-  );
+  await authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+  await authenticate("signInWithPassword", email, password);
 }
